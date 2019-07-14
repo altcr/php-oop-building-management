fd23c2ea86331b6259475c5bc5806afafeb0a4ec
Ali TECER <171320151036@ogrenci.ogu.edu.tr>
2019-07-14 15:33:08 +0300

    File modified.

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ require_once "class.person.php";
 
 $person = new Person;
 
-$person->setApartAddress("Eskişehir");
-$person->setApartName("Köşkdağı");
-$person->setApartNo(36);
-$person->setApartNumber(20);
-$person->setPersonName("Ali TECER");
-$person->setPersonTel("555 055 0055");
-$person->setPersonMail("m.tecer97@hotmail.com");
-$person->setPersonFlatNo("3");
+$person->setApartAddress("#");
+$person->setApartName("#");
+$person->setApartNo("#");
+$person->setApartNumber("#");
+$person->setPersonName("#");
+$person->setPersonTel("#");
+$person->setPersonMail("#");
+$person->setPersonFlatNo("#");
 
 $info = [
     "Apartment Name" => $person->getApartName(),
